d6f254d078ac2ed162cd51c19831851e179f03c6
Léu Almeida <leo@webid.net.br>
2019-08-04 21:48:51 -0300

    First commit! :alien:

---
+++ frontend/src/template/menu.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default props => (
+    <nav className='navbar navbar-inverse bg-inverse'>
+        <div className='container'>
+            <div className='navbar-header'>
+                <a className='navbar-brand' href='#'>
+                    <i className='fa fa-user'></i> Léu Almeida
+                </a>
+            </div>
+
+            <div id='navbar' className='navbar-collapse collapse'>
+                <ul className='nav navbar-nav'>
+                    <li><a href='#/todos'>Tarefas</a></li>
+                    <li><a href='#/about'>Sobre</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+)
